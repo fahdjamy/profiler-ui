b4703b4eff5c809cf7ec09593fdd0996f5d64b51
Benard <benardjovita2012@gmail.com>
2022-11-27 23:48:02 +0300

    implemented routing functionality

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import Layout from '../../components/Layout'
+
+const Contact: React.FC = () => {
+  return (
+    <Layout>
+        <Fragment>
+            <div>
+            <h2>Contact us</h2>
+            </div>
+        </Fragment>
+    </Layout>
+  )
+}
+
+export default Contact
